9bb8e4b17bc30530140480f0a9fc8d8611b27bf3
yanzhenyidai <frank-tan@outlook.com>
2025-10-21 11:21:00 +0800

    增加枚举的attr属性

--- src/main/java/cn/xinyue365/ocr/v20250408/models/invoice/enums/InvoiceTypeEnum.java
+++ src/main/java/cn/xinyue365/ocr/v20250408/models/invoice/enums/InvoiceTypeEnum.java
@@ -7,33 +7,36 @@ package cn.xinyue365.ocr.v20250408.models.invoice.enums;
  */
 public enum InvoiceTypeEnum {
 
-    ELECTRONIC_ORDINARY_INVOICE("电子普通发票", "10102"),
-    DIGITAL_ELECTRONIC_ORDINARY_INVOICE("数电普通发票", "10108"),
-    ORDINARY_INVOICE("普通发票", "10101"),
-    SPECIAL_INVOICE("专用发票", "10100"),
-    ELECTRONIC_SPECIAL_INVOICE("电子专用发票", "10100a"),
-    DIGITAL_ELECTRONIC_SPECIAL_INVOICE("数电专用发票", "10107"),
-    GENERAL_INVOICE("通用发票", "10101a"),
-    TRAIN_TICKET("火车票", "10503"),
-    QUOTA_INVOICE("定额发票", "10200"),
-    MOTOR_VEHICLE_INVOICE("机动车销售统一发票", "10104"),
-    AIR_TICKET_ITINERARY("机票行程单", "10506"),
-    ROLL_INVOICE("卷票", "10103"),
-    TAX_PAYMENT_CERTIFICATE("税收完税证明", "10902"),
-    ROAD_BRIDGE_FEE_INVOICE("过路过桥费发票", "10507"),
-    SECOND_HAND_CAR_INVOICE("二手车发票", "10105"),
-    PASSENGER_TRANSPORT_TICKET("客运车船票", "10505a"),
-    TAXI_RECEIPT("出租车票", "10500"),
-    MACHINE_PRINTED_INVOICE("机打发票", "10400"),
-    OTHER_INVOICE("其他发票", "10900");
+    ELECTRONIC_ORDINARY_INVOICE("电子普通发票", "10102", "vatInvoice"),
+    DIGITAL_ELECTRONIC_ORDINARY_INVOICE("数电普通发票", "10108", "vatInvoice"),
+    ORDINARY_INVOICE("普通发票", "10101", "vatInvoice"),
+    SPECIAL_INVOICE("专用发票", "10100", "vatInvoice"),
+    ELECTRONIC_SPECIAL_INVOICE("电子专用发票", "10100a", "vatInvoice"),
+    DIGITAL_ELECTRONIC_SPECIAL_INVOICE("数电专用发票", "10107", "vatInvoice"),
+    GENERAL_INVOICE("通用发票", "10101a", "vatInvoice"),
+    TRAIN_TICKET("火车票", "10503", "trainInvoice"),
+    QUOTA_INVOICE("定额发票", "10200", "quotaInvoice"),
+    MOTOR_VEHICLE_INVOICE("机动车销售统一发票", "10104", "motorVehicleInvoice"),
+    AIR_TICKET_ITINERARY("机票行程单", "10506", "airInvoice"),
+    ROLL_INVOICE("卷票", "10103", "couponInvoice"),
+    TAX_PAYMENT_CERTIFICATE("税收完税证明", "10902", "taxProofInvoice"),
+    ROAD_BRIDGE_FEE_INVOICE("过路过桥费发票", "10507", "roadTaxInvoice"),
+    SECOND_HAND_CAR_INVOICE("二手车发票", "10105", "usedCarInvoice"),
+    PASSENGER_TRANSPORT_TICKET("客运车船票", "10505a", "shipInvoice"),
+    TAXI_RECEIPT("出租车票", "10500", "taxiInvoice"),
+    MACHINE_PRINTED_INVOICE("机打发票", "10400", "handwrittenInvoice"),
+    OTHER_INVOICE("其他发票", "10900", "otherInvoice");
 
     private final String code;
 
     private final String value;
 
-    InvoiceTypeEnum(String code, String value) {
+    private final String attr;
+
+    InvoiceTypeEnum(String code, String value, String attr) {
         this.code = code;
         this.value = value;
+        this.attr = attr;
     }
 
     public String getCode() {
@@ -42,6 +45,10 @@ public enum InvoiceTypeEnum {
 
     public String getValue() {
         return value;
+    }
+
+    public String getAttr() {
+        return attr;
     }
 
     public static String getValueByCode(String code) {
@@ -53,10 +60,28 @@ public enum InvoiceTypeEnum {
         return null;
     }
 
+    public static String getAttrByCode(String code) {
+        for (InvoiceTypeEnum invoiceTypeEnum : InvoiceTypeEnum.values()) {
+            if (invoiceTypeEnum.getCode().equals(code)) {
+                return invoiceTypeEnum.getAttr();
+            }
+        }
+        return null;
+    }
+
     public static String getCodeByValue(String value) {
         for (InvoiceTypeEnum invoiceTypeEnum : InvoiceTypeEnum.values()) {
             if (invoiceTypeEnum.getValue().equals(value)) {
                 return invoiceTypeEnum.getCode();
+            }
+        }
+        return null;
+    }
+
+    public static String getAttrByValue(String value) {
+        for (InvoiceTypeEnum invoiceTypeEnum : InvoiceTypeEnum.values()) {
+            if (invoiceTypeEnum.getValue().equals(value)) {
+                return invoiceTypeEnum.getAttr();
             }
         }
         return null;
