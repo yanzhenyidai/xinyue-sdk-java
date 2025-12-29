0746f3dc1c6f28a0b136acd346be162ed7bb761f
yanzhenyidai <frank-tan@outlook.com>
2025-12-29 17:43:18 +0800

    版本升级1.0.12

--- src/main/java/cn/xinyue365/common/http/HttpConnection.java
+++ src/main/java/cn/xinyue365/common/http/HttpConnection.java
@@ -10,21 +10,39 @@ import java.util.concurrent.TimeUnit;
 public class HttpConnection {
 
     private static final Logger logger = Logger.getLogger(HttpConnection.class);
-
-    private static final OkHttpClient clientSingleton = new OkHttpClient();
+    private static volatile OkHttpClient clientSingleton;
     private final OkHttpClient client;
 
     public HttpConnection(Integer connTimeout, Integer readTimeout, Integer writeTimeout) {
-        this.client = clientSingleton.newBuilder()
+        this.client = getClientSingleton()
+            .newBuilder()
             .connectTimeout(connTimeout, TimeUnit.SECONDS)
             .readTimeout(readTimeout, TimeUnit.SECONDS)
             .writeTimeout(writeTimeout, TimeUnit.SECONDS)
             .build();
     }
 
+    private static OkHttpClient getClientSingleton() {
+        if (clientSingleton == null) {
+            synchronized (HttpConnection.class) {
+                if (clientSingleton == null) {
+                    try {
+                        clientSingleton = new OkHttpClient();
+                    } catch (Throwable t) {
+                        Logger.getLogger(HttpConnection.class)
+                            .error("Init OkHttpClient failed", t);
+                        throw t;
+                    }
+                }
+            }
+        }
+        return clientSingleton;
+    }
+
     public String doRequest(Request request) throws IOException {
         int retries = 3;
         IOException lastException = null;
+
         for (int i = 0; i < retries; i++) {
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
@@ -33,7 +51,7 @@ public class HttpConnection {
                 return response.body().string();
             } catch (IOException e) {
                 lastException = e;
-                logger.error("Request attempt " + (i + 1) + " failed: " + e.getMessage());
+                logger.error("Request attempt " + (i + 1) + " failed", e);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException ignored) {
@@ -44,89 +62,82 @@ public class HttpConnection {
     }
 
     public String getRequest(String url) throws SDKException, IOException {
-        Request request = null;
         try {
-            request = new Request.Builder().url(url).get().build();
+            Request request = new Request.Builder().url(url).get().build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 
     public String getRequest(String url, Headers headers) throws SDKException, IOException {
-        Request request = null;
         try {
-            request = new Request.Builder().url(url).headers(headers).get().build();
+            Request request =
+                new Request.Builder().url(url).headers(headers).get().build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 
     public String postRequest(String url, String body) throws SDKException, IOException {
-        MediaType contentType = MediaType.parse("application/x-www-form-urlencoded");
-        Request request = null;
         try {
-            request = new Request.Builder().url(url).post(RequestBody.create(body, contentType)).build();
+            MediaType contentType = MediaType.parse("application/x-www-form-urlencoded");
+            Request request =
+                new Request.Builder()
+                    .url(url)
+                    .post(RequestBody.create(body, contentType))
+                    .build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 
     public String postRequest(String url, String body, Headers headers)
         throws SDKException, IOException {
-        MediaType contentType = MediaType.parse(headers.get("Content-Type"));
-        Request request;
         try {
-            request =
+            MediaType contentType = MediaType.parse(headers.get("Content-Type"));
+            Request request =
                 new Request.Builder()
                     .url(url)
                     .post(RequestBody.create(body, contentType))
                     .headers(headers)
                     .build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 
     public String postRequest(String url, byte[] body, Headers headers)
         throws SDKException, IOException {
-        MediaType contentType = MediaType.parse(headers.get("Content-Type"));
-        Request request = null;
         try {
-            request =
+            MediaType contentType = MediaType.parse(headers.get("Content-Type"));
+            Request request =
                 new Request.Builder()
                     .url(url)
                     .post(RequestBody.create(body, contentType))
                     .headers(headers)
                     .build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 
     public String postRequest(String url, RequestBody body, Headers headers)
         throws SDKException, IOException {
-        Request request;
         try {
-            request =
+            Request request =
                 new Request.Builder()
                     .url(url)
                     .post(body)
                     .headers(headers)
                     .build();
+            return this.doRequest(request);
         } catch (IllegalArgumentException e) {
             throw new SDKException(e.getClass().getName() + "-" + e.getMessage());
         }
-
-        return this.doRequest(request);
     }
 }
